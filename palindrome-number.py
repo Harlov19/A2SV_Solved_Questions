82e33eb9c9ac56660a1a4d86aa6be9edf014b8c2
Misganaw <misganawa956@gmail.com>
2026-02-03 14:52:36 +0300

    solution

---
+++ palindrome-number.py
@@ -0,0 +1,17 @@
+"""
+Leetcode 9 Palindrom number
+Given an integer x, return true if x is a palindrome, and false otherwise.
+"""
+class Solution(object):
+    def isPalindrome(self, x):
+        x = str(x)
+        left = 0
+        right = len(x)-1
+        while(left<right):
+            if(x[left] !=x[right]):
+                return False
+            else:
+                left+=1
+                right-=1
+        return True
+        
