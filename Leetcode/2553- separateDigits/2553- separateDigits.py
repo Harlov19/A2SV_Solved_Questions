8cbb937e8ba338e572cc834a73972d6c45829335
Misganaw <misganawa956@gmail.com>
2026-02-09 23:17:55 +0300

    solution

--- Leetcode/2553- separateDigits/2553- separateDigits.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def separateDigits(self, nums: List[int]) -> List[int]:
-        def getDigit(num):
-            return [ int(digit) for digit in str(num)]
-        ans = []
-        for num in nums:
-            ans.extend(getDigit(num))
-        return ans
