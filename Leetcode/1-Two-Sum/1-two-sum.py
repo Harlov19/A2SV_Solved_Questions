7acfb20351b40beb8dee030848110201ac9f52a0
Misganaw <misganawa956@gmail.com>
2026-02-09 23:23:12 +0300

    updating file structure

---
+++ Leetcode/1-Two-Sum/1-two-sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_index = {}
+        for i,num in enumerate(nums):
+            if(target-num in num_index):
+                return [i,num_index[target-num]]
+            num_index[num] = i
+       
