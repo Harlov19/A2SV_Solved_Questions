3370fa646bacd2696d0147b898c5d9d85d0d0101
Misganaw <misganawa956@gmail.com>
2026-02-09 14:59:56 +0300

    solution

--- Leetcode/136-Single Number/singleNumber.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        counts = Counter(nums)
-        for num in nums:
-            if counts[num]==1:
-                return num
