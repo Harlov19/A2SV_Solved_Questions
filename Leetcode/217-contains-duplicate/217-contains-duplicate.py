e2a98f304a6a29e2d6167d65b466139af8dafd80
Misganaw <misganawa956@gmail.com>
2026-02-12 21:31:26 +0300

    solution

---
+++ Leetcode/217-contains-duplicate/217-contains-duplicate.py
@@ -0,0 +1,7 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        counts = Counter(nums)
+        for num in counts:
+            if(counts[num]>=2):
+                return True
+        return False
