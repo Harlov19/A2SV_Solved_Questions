2635743d60589ff375637dde1e88357588ee75b5
Misganaw <misganawa956@gmail.com>
2026-02-13 20:58:35 +0300

    Implement matrix transposition in new file

---
+++ Leetcode/867-transpose-matrix.py
@@ -0,0 +1,8 @@
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        transposed = [[0 for _ in range(len(matrix))] for _ in range(len(matrix[0]))]
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                transposed[col][row] = matrix[row][col]
+        return transposed
+        
