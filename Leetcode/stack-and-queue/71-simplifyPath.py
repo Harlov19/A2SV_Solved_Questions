46d2a90da77c8ca3849aa4f52235d95d3ac7b8b5
Misganaw <misganawa956@gmail.com>
2026-03-09 22:49:21 +0300

    Create 71-simplifyPath.py

---
+++ Leetcode/stack-and-queue/71-simplifyPath.py
@@ -0,0 +1,13 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        path = path.split('/')
+        print(path)
+        stack = []
+        for c in path:
+            if c == "" or c == '.':
+                continue
+            elif c == '..':
+                if stack:stack.pop() 
+            else:
+                stack.append(c)
+        return   '/'+ "/".join(stack)
