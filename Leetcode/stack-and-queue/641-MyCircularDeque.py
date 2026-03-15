2fd6e7d1e90a99c088b800293e4ef5bacf3f7ae3
Misganaw <misganawa956@gmail.com>
2026-03-15 16:46:27 +0300

    Create 641-MyCircularDeque.py

---
+++ Leetcode/stack-and-queue/641-MyCircularDeque.py
@@ -0,0 +1,59 @@
+class MyCircularDeque:
+
+    def __init__(self, k: int):
+        self.cap = k + 1
+        self.q = [0] * self.cap
+        self.front = 0
+        self.rear = 0
+
+    def insertFront(self, value: int) -> bool:
+        if self.isFull():
+            return False
+        self.front = (self.front - 1 + self.cap) % self.cap
+        self.q[self.front] = value
+        return True
+
+    def insertLast(self, value: int) -> bool:
+        if self.isFull():
+            return False
+        self.q[self.rear] = value
+        self.rear = (self.rear + 1) % self.cap
+        return True
+
+    def deleteFront(self) -> bool:
+        if self.isEmpty():
+            return False
+        self.front = (self.front + 1) % self.cap
+        return True
+
+    def deleteLast(self) -> bool:
+        if self.isEmpty():
+            return False
+        self.rear = (self.rear - 1 + self.cap) % self.cap
+        return True
+
+    def getFront(self) -> int:
+        if self.isEmpty():
+            return -1
+        return self.q[self.front]
+
+    def getRear(self) -> int:
+        if self.isEmpty():
+            return -1
+        return self.q[(self.rear - 1 + self.cap) % self.cap]
+
+    def isEmpty(self) -> bool:
+        return self.front == self.rear
+
+    def isFull(self) -> bool:
+        return (self.rear + 1) % self.cap == self.front
+# Your MyCircularDeque object will be instantiated and called as such:
+# obj = MyCircularDeque(k)
+# param_1 = obj.insertFront(value)
+# param_2 = obj.insertLast(value)
+# param_3 = obj.deleteFront()
+# param_4 = obj.deleteLast()
+# param_5 = obj.getFront()
+# param_6 = obj.getRear()
+# param_7 = obj.isEmpty()
+# param_8 = obj.isFull()
