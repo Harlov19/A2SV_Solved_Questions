679bdd31b5085a83b0e2be29e13d52881eb9def1
Misganaw <misganawa956@gmail.com>
2026-03-30 18:00:27 +0300

    Refactor combine method for improved clarity
    Refactor combine method to use a different backtracking function and adjust loop range.

--- Leetcode/RecursionII/77- combine.py
+++ Leetcode/RecursionII/77- combine.py
@@ -1,17 +1,14 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        res = []
-        path = []
-
-        def backtrack(start: int):
-            if len(path) == k:
-                res.append(path.copy())
-                return
-
-            for i in range(start, n - (k - len(path)) + 2):
-                path.append(i)
-                backtrack(i + 1)
-                path.pop()
-
-        backtrack(1)
-        return res
+       path = []
+       res = []
+       def bct(l):
+        if len(path) == k:
+            res.append(path.copy())
+            return
+        for i in range(l,n+1):
+            path.append(i)
+            bct(i+1)
+            path.pop()
+       bct(1)
+       return res
