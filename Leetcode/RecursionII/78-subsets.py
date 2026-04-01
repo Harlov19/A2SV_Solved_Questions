74edaaa210ff41a2fcd0bec8ff0fdfa326d67cd9
Misganaw <misganawa956@gmail.com>
2026-04-01 20:21:37 +0300

    Implement subsets function using backtracking

---
+++ Leetcode/RecursionII/78-subsets.py
@@ -0,0 +1,14 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        ans = [[]]
+        sub = []
+        n = len(nums)
+        def bct(l):
+            for i in range(l,n):
+                sub.append(nums[i])
+                ans.append(sub.copy())
+                bct(i+1)
+                sub.pop()
+             
+        bct(0)
+        return ans
