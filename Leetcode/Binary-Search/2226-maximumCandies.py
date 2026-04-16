c8db6b0123bf0fcef5f80864800673024433785e
Misganaw <misganawa956@gmail.com>
2026-04-16 23:19:14 +0300

    Create 2226-maximumCandies.py

---
+++ Leetcode/Binary-Search/2226-maximumCandies.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maximumCandies(self, candies: List[int], k: int) -> int:
+        
+        def can(x):
+            return sum(c // x for c in candies) >= k
+        
+        left, right = 1, max(candies)
+        ans = 0
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if can(mid):
+                ans = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return ans
