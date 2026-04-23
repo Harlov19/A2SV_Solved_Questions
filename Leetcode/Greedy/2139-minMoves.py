d591a1170b46b49d313e53b440c5779b60bfd0fb
Misganaw <misganawa956@gmail.com>
2026-04-23 20:01:44 +0300

    Refactor minMoves function for clarity and efficiency

--- Leetcode/Greedy/2139-minMoves.py
+++ Leetcode/Greedy/2139-minMoves.py
@@ -1,15 +1,11 @@
 class Solution:
     def minMoves(self, target: int, maxDoubles: int) -> int:
-        res = 0
-        while  target > 1:
-            if maxDoubles > 0  and target%2 == 0:
+        moves = 0
+        while  target > 1 and maxDoubles > 0 :
+            if target%2 == 0:
                 target = target//2
                 maxDoubles-=1
             else:
-                if maxDoubles == 0:
-                    res += target - 1
-                    break
-                else:
-                    target-=1
-            res+=1
-        return res 
+                target-=1
+            moves+=1
+        return moves + (target-1)
