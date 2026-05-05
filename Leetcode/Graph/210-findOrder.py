28a93bb478970e094eb640ac2a9e29acaba5d294
Misganaw <misganawa956@gmail.com>
2026-05-05 18:38:03 +0300

    Refactor findOrder to use Kahn's algorithm
    Refactored the findOrder function to use Kahn's algorithm for topological sorting, replacing the DFS approach. Added indegree tracking and queue for processing nodes.

--- Leetcode/Graph/210-findOrder.py
+++ Leetcode/Graph/210-findOrder.py
@@ -1,30 +1,22 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         graph = defaultdict(list)
+        indegree = [0]*numCourses
         for a,b in prerequisites:
             graph[b].append(a)
-
-        visited = set()
-        inStack = set()
-
+            indegree[a]+=1
+        
+        q = deque([i for i in range(numCourses) if indegree[i] == 0])
         order = []
-        def dfs(node):
-            if node in inStack:
-                return True
-            if node in visited:
-                return False
-            visited.add(node)
-            inStack.add(node)
-
-            for nei in graph[node]:
-                if dfs(nei):
-                    return True
-            order.append(node)
-            inStack.remove(node)
-            return False
-        for i in range(numCourses):
-            if i not in visited:
-                if dfs(i):
-                    return []
-       
-        return order[::-1]
+        
+        while q:
+            curr = q.popleft()
+            order.append(curr)
+            for nei in graph[curr]:
+                indegree[nei]-=1
+                if indegree[nei] == 0:
+                    q.append(nei)
+        if len(order) == numCourses:
+            return order
+        else:
+            return []
