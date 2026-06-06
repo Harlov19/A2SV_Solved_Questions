5132dae1150ed5a4772f5cf587e5dc8ef169a2b2
Misganaw <misganawa956@gmail.com>
2026-06-06 23:08:01 +0300

    change attribute
    Corrected spacing in TreeNode class definition.

--- Leetcode/Graph/102-levelOrder.py
+++ Leetcode/Graph/102-levelOrder.py
@@ -2,7 +2,7 @@
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
-#         self.left = left
+#         self.left =  left
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
